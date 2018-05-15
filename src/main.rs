e46b6d03bf2235544414847fddeb4f7eba0cdece
Marc-André Moreau <mamoreau@devolutions.net>
2018-05-15 15:51:00 -0400

    ceviche: start Service class

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,44 @@ use libc::{c_int, c_char};
 
 pub enum NtService {}
 
-extern "C" {
+extern {
 	pub fn NtService_ProcessCommandLine(ctx: *mut NtService, argc: c_int, argv: *const *const c_char) -> bool;
 	pub fn NtService_New(serviceName: *const c_char, displayName: *const c_char, description: *const c_char) -> *mut NtService;
 	pub fn NtService_Free(ctx: *mut NtService);
 }
 
-extern "C" {
-    fn main_c(argc: c_int, argv: *const *const c_char) -> c_int;
+pub struct Service {
+    ctx: *mut NtService
 }
 
-fn main_rs() {
-	unsafe {
-		let service_name = CString::new("foobar").unwrap();
-		let display_name = CString::new("FooBar Service").unwrap();
-		let description = CString::new("This is the FooBar service").unwrap();
-		let service = NtService_New(service_name.as_ptr(), display_name.as_ptr(), description.as_ptr());
+impl Service {
+    pub fn new(service_name: &str, display_name: &str, description: &str) -> Option<Service> {
+        let raw_service = unsafe {
+			let service_name = CString::new(service_name).unwrap();
+			let display_name = CString::new(display_name).unwrap();
+			let description = CString::new(description).unwrap();
+			NtService_New(service_name.as_ptr(), display_name.as_ptr(), description.as_ptr())
+        };
 
-    	let args: Vec<CString> = env::args().filter_map(|arg| CString::new(arg).ok()).collect();
+        let args: Vec<CString> = env::args().filter_map(|arg| CString::new(arg).ok()).collect();
     	let c_args: Vec<*const c_char> = args.iter().map(|arg| arg.as_ptr()).collect();
 
-    	NtService_ProcessCommandLine(service, c_args.len() as c_int, c_args.as_ptr());
+    	unsafe {
+    		NtService_ProcessCommandLine(raw_service, c_args.len() as c_int, c_args.as_ptr());
+    	};
 
-		NtService_Free(service);
-	};
+        Some(Service { ctx: raw_service })
+    }
+}
+
+impl Drop for Service {
+	fn drop(&mut self) {
+        unsafe {
+            NtService_Free(self.ctx);
+        }
+    }
 }
 
 fn main() {
-	main_rs();
-	return;
-
-    let args: Vec<CString> = env::args().filter_map(|arg| CString::new(arg).ok()).collect();
-    let c_args: Vec<*const c_char> = args.iter().map(|arg| arg.as_ptr()).collect();
-
-    unsafe {
-        main_c(c_args.len() as c_int, c_args.as_ptr());
-    };
+	let service = Service::new("foobar", "FooBar Service", "This is the FooBar service");
 }
